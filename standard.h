d0edd70846c934f905449a206492c3afa4fa70fc
seochangLivKim <audwnssp@naver.com>
2023-12-14 03:02:19 +0900

    2nd commit

--- standard.h
+++ standard.h
@@ -37,19 +37,19 @@
 // 색상 정의
 #define BLACK	0
 #define BLUE1	1
-#define GREEN1	2
+#define GREEN1 2
 #define CYAN1	3
 #define RED1	4
 #define MAGENTA1 5
-#define YELLOW1	6
+#define YELLOW1 6
 #define GRAY1	7
 #define GRAY2	8
 #define BLUE2	9
-#define GREEN2	10
+#define GREEN2 10
 #define CYAN2	11
 #define RED2	12
 #define MAGENTA2 13
-#define YELLOW2	14
+#define YELLOW2 14
 #define WHITE	15
 
 #define STAR1 'O' // player1 표시
